6c6667e0744c3248f30a1b3900db2aa77f16da6b
Tom Barrett <thomasjbarrett@live.com>
2019-10-25 05:21:58 -0500

    Added test for toolbox dbcontext

---
+++ PFCToolbox.Test/Data/PFCToolboxContextTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PFCToolbox.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFCToolbox.Test.Data
+{
+    [TestClass]
+    public class PFCToolboxContextTests
+    {
+        [TestMethod]
+        public void PFCToolbox_ConnectToContext()
+        {
+            var context = new PFCToolboxContext();
+            var result = context.Database;
+
+            Assert.IsNotNull(result);
+
+            context.Dispose();
+        }
+    }
+}
