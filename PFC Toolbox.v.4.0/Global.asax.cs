fe065cbbfa98820ced1596280d5ffbf3283082f8
Tom Barrett <thomasjbarrett@live.com>
2019-10-27 07:01:15 -0500

    Adding auxiliary service and dependency injections

--- PFC Toolbox.v.4.0/Global.asax.cs
+++ PFC Toolbox.v.4.0/Global.asax.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using PFCToolbox.Service;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
+using System.Reflection;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -18,6 +19,18 @@ namespace PFC_Toolbox.v._4._0
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // set up SimpleInjector for dependency injections
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+
+            container.Register<IAuxiliaryService, AuxiliaryService>(Lifestyle.Scoped);
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+
+            container.Verify();
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
         }
     }
 }
---
+++ PFCToolbox.Service/AuxiliaryService.cs
@@ -0,0 +1,36 @@
+﻿using PFCToolbox.Common.Model;
+using PFCToolbox.Data.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFCToolbox.Service
+{
+    public interface IAuxiliaryService
+    {
+        //public IEnumerable<Location> GetAllLocations();
+        //public IEnumerable<Subdepartment> GetAllSubdepartments();
+        public IEnumerable<Vendor> GetAllVendors();
+    }
+
+    public class AuxiliaryService : IAuxiliaryService
+    {
+        private readonly IRepo<Location> _locationRepo;
+        private readonly IRepo<Subdepartment> _subdeptRepo;
+        private readonly IRepo<Vendor> _vendorRepo;
+
+        public AuxiliaryService(IRepo<Location> locRepo, IRepo<Subdepartment> subRepo, IRepo<Vendor> vendRepo)
+        {
+            _locationRepo = locRepo;
+            _subdeptRepo = subRepo;
+            _vendorRepo = vendRepo;
+        }
+
+        public IEnumerable<Vendor> GetAllVendors()
+        {
+            return _vendorRepo.GetAll();
+        }
+    }
+}
