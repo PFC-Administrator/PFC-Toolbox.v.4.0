fa8db6b31729911c6dae9523c6e27ac9e485e054
Jeremy Anderson <52007940+SnackMeUp@users.noreply.github.com>
2020-01-13 14:31:20 -0600

    Check if UPC exists in SMS

---
+++ PFC Toolbox.v.4.0/Controllers/Maintenance/UPCController.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using System.Web;
+using System.Web.Http;
+using DataTables;
+
+namespace PFC_Toolbox.v._4._0.Controllers
+{
+    public class UPCController : ApiController
+    {
+        [Route("Maintenance/api/CheckUPC")]
+        [HttpGet]
+        public IHttpActionResult ProductUpdates()
+        {
+            var request = HttpContext.Current.Request;
+
+            using (var db1 = new Database("sqlserver", ConfigurationManager.ConnectionStrings["SMSHostConnection"].ConnectionString))
+            {
+                var response = new Editor(db1, "OBJ_TAB", "F01")
+                    .Field(new Field("OBJ_TAB.F01")
+                    )
+                    .Process(request)
+                    .Data();
+
+                return Json(response);
+
+            }
+        }
+    }
+}
