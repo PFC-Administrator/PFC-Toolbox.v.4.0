996c03c08cff500949dcf0137c6c19d9ac546e89
Jeremy Anderson <52007940+SnackMeUp@users.noreply.github.com>
2020-01-28 14:36:22 -0600

    Brand report (#12)
    * Finalized Item Single Total by Brand Report.
    
    * Update .gitignore

---
+++ PFC Toolbox.v.4.0/Models/Reports/ItemSingleTotalbyBrandModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PFC_Toolbox.v._4._0.Models
+{
+    public class ItemSingleTotalbyBrandModel
+    {
+        [DisplayAttribute(Name = "UPC/PLU")]
+        public string ItemCode { get; set; }
+
+        [DisplayAttribute(Name = "Subdepartment")]
+        public string Subdept { get; set; }
+
+        [DisplayAttribute(Name = "Brand")]
+        public string ItemBrand { get; set; }
+
+        [DisplayAttribute(Name = "Description")]
+        public string ItemDescription { get; set; }
+
+        [DisplayAttribute(Name = "Size")]
+        public string ItemSize { get; set; }
+
+        [DisplayAttribute(Name = "Weight")]
+        public double SalesWeight { get; set; }
+
+        [DisplayAttribute(Name = "Total Sales")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public Decimal SalesTotal { get; set; }
+
+        [DisplayAttribute(Name = "Quantity")]
+        [DisplayFormat(DataFormatString = "{0}")]
+        public double SalesQuantity { get; set; }
+    }
+}
