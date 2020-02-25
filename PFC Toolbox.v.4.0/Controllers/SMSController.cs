0302cd5d64b483a03e3143daf03ccca9f74d5c7b
Jeremy Anderson <Jeremy.Anderson@pfc.coop>
2020-02-25 17:02:55 -0600

    Item Lookup by Brand

--- PFC Toolbox.v.4.0/Controllers/SMSController.cs
+++ PFC Toolbox.v.4.0/Controllers/SMSController.cs
@@ -254,7 +254,7 @@ namespace PFC_Toolbox.v._4._0.Controllers
                                 // Temp variables for TryParse
                                 decimal tempDecimal;
 
-                                // Store results in PurchasesSummaryModel model
+                                // Store results in ItemLookupByDescriptionModel model
                                 item = new ItemLookupByDescriptionModel();
                                 item.ItemCode = reader["ItemCode"].ToString();
                                 item.ItemBrand = reader["Brand"].ToString();
@@ -389,7 +389,7 @@ namespace PFC_Toolbox.v._4._0.Controllers
                                 // Temp variables for TryParse
                                 decimal tempDecimal;
 
-                                // Store results in PurchasesSummaryModel model
+                                // Store results in ItemLookupByDescriptionyModel model
                                 item = new ItemLookupByDescriptionModel();
                                 item.ItemCode = reader["ItemCode"].ToString();
                                 item.ItemBrand = reader["Brand"].ToString();
@@ -495,6 +495,302 @@ namespace PFC_Toolbox.v._4._0.Controllers
                 ViewBag.store = storeDescription;
 
                 return PartialView("_ItemLookupByDescription", report);
+            }
+            else
+            {
+                Response.Redirect("Error");
+                return null;
+            }
+        }
+
+        /*******************************************************************************************************************************************************************************************************************/
+
+        // GET: /SMS/ItemLookupByBrand
+        public ActionResult ItemLookupByBrand()
+        {
+            return View();
+        }
+
+        public ActionResult GetItemCodeInfoByBrand(string brand, string location)
+        {
+            // Creates variables for use within stored procedure and report display
+            string storeCode = location.Split(',')[0];
+            string storeDescription = location.Split(',')[1];
+
+            // Create local objects
+            SqlDataReader reader = null;
+            List<ItemLookupByBrandModel> report = new List<ItemLookupByBrandModel>();
+            ItemLookupByBrandModel item = null;
+
+            if (location.Equals("001, La Crosse"))
+            {
+                // Connect to Host SMS and run Toolbox-ItemLookupByBrand stored procedure
+                using (SqlConnection con = new SqlConnection { ConnectionString = ConfigurationManager.ConnectionStrings["SMSLaxConnection"].ConnectionString })
+                {
+                    using (SqlCommand cmd = new SqlCommand("[Toolbox-ItemLookupByBrand]", con))
+                    {
+                        // Set the command type as a stored procedure
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Set the stored procedure parameters
+                        cmd.Parameters.Add("@brand", SqlDbType.VarChar).Value = brand;
+
+                        // Open connection to SQL server and set a timeout of 1000 in case report takes a while
+                        con.Open();
+                        cmd.CommandTimeout = 1000;
+
+                        // Execute cmd against server and store in reader object
+                        reader = cmd.ExecuteReader();
+
+                        // Save query results to list
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                // Temp variables for TryParse
+                                decimal tempDecimal;
+
+                                // Store results in ItemLookupByBrandModel model
+                                item = new ItemLookupByBrandModel();
+                                item.ItemCode = reader["ItemCode"].ToString();
+                                item.ItemBrand = reader["Brand"].ToString();
+                                item.ItemDescription = reader["ReceiptDescription"].ToString();
+                                item.ItemLongDesc = reader["LongDescription"].ToString();
+                                item.ItemSize = reader["Size"].ToString();
+                                item.ItemCategory = reader["Category"].ToString();
+                                item.ItemReport = reader["Report"].ToString();
+                                item.ItemSubdepartment = reader["Subdepartment"].ToString();
+
+                                if (reader["Scalable"].ToString().Equals("0") || reader["Scalable"].ToString().Equals(""))
+                                {
+                                    item.ItemScaled = "No";
+                                }
+                                else if (reader["Scalable"].ToString().Equals("1"))
+                                {
+                                    item.ItemScaled = "Yes";
+                                }
+
+                                if (reader["Tax"].ToString().Equals("0") || reader["Tax"].ToString().Equals(""))
+                                {
+                                    item.ItemTaxed = "No";
+                                }
+                                else if (reader["Tax"].ToString().Equals("1"))
+                                {
+                                    item.ItemTaxed = "Yes";
+                                }
+
+                                if (reader["FoodStamp"].ToString().Equals("0") || reader["FoodStamp"].ToString().Equals(""))
+                                {
+                                    item.ItemFoodStamp = "No";
+                                }
+                                else if (reader["Tax"].ToString().Equals("1"))
+                                {
+                                    item.ItemFoodStamp = "Yes";
+                                }
+
+                                if (Decimal.TryParse(reader["RegularPrice"].ToString(), out tempDecimal))
+                                {
+                                    item.ItemRegPrice = tempDecimal;
+                                }
+                                else item.ItemRegPrice = 0;
+
+                                if (!reader["TPRPrice"].ToString().Equals(""))
+                                {
+                                    if (Decimal.TryParse(reader["TPRPrice"].ToString(), out tempDecimal))
+                                    {
+                                        item.ItemTPR = tempDecimal;
+                                    }
+                                    else item.ItemTPR = 0;
+
+                                    item.ItemStartTPR = reader["TPRStart"].ToString();
+                                    item.ItemEndTPR = reader["TPREnd"].ToString();
+                                    if (Decimal.TryParse(reader["PackageTPR"].ToString(), out tempDecimal))
+                                    {
+                                        item.ItemPkgTPR = tempDecimal;
+                                    }
+                                    else item.ItemPkgTPR = 0;
+                                }
+
+                                if (Decimal.TryParse(reader["PackagePrice"].ToString(), out tempDecimal))
+                                {
+                                    item.ItemPkgPrice = tempDecimal;
+                                }
+                                else item.ItemPkgPrice = 0;
+
+                                if (!reader["ActiveVendor"].ToString().Equals(""))
+                                {
+                                    item.ItemVendor = reader["ActiveVendor"].ToString();
+                                    if (!reader["VendorCode"].ToString().Equals(""))
+                                    {
+                                        item.ItemVendorCode = reader["VendorCode"].ToString();
+                                    }
+                                    if (!reader["Cost"].ToString().Equals(""))
+                                    {
+                                        if (Decimal.TryParse(reader["Cost"].ToString(), out tempDecimal))
+                                        {
+                                            item.ItemCost = tempDecimal;
+                                        }
+                                        else item.ItemCost = 0;
+                                        if (item.ItemCost != 0)
+                                        {
+                                            if (item.ItemRegPrice != 0)
+                                            {
+                                                item.ItemMargin = ((item.ItemRegPrice - item.ItemCost) / item.ItemRegPrice) * 100;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                // Add results to list
+                                report.Add(item);
+                            }
+                        }
+                        else item.ItemCode = "Item Not Found";
+
+                        // Close connection to SQL server
+                        con.Close();
+                    }
+                }
+
+                // Add totals to ViewBag to be used in report display
+                ViewBag.store = storeDescription;
+
+                return PartialView("_ItemLookupByBrand", report);
+            }
+            else if (location.Equals("002, Rochester"))
+            {
+                // Connect to Host SMS and run Toolbox-ItemLookupByBrand stored procedure
+                using (SqlConnection con = new SqlConnection { ConnectionString = ConfigurationManager.ConnectionStrings["SMSRochConnection"].ConnectionString })
+                {
+                    using (SqlCommand cmd = new SqlCommand("[Toolbox-ItemLookupByBrand]", con))
+                    {
+                        // Set the command type as a stored procedure
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Set the stored procedure parameters
+                        cmd.Parameters.Add("@brand", SqlDbType.VarChar).Value = brand;
+
+                        // Open connection to SQL server and set a timeout of 1000 in case report takes a while
+                        con.Open();
+                        cmd.CommandTimeout = 1000;
+
+                        // Execute cmd against server and store in reader object
+                        reader = cmd.ExecuteReader();
+
+                        // Save query results to list
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                // Temp variables for TryParse
+                                decimal tempDecimal;
+
+                                // Store results in ItemLookupByBrandModel model
+                                item = new ItemLookupByBrandModel();
+                                item.ItemCode = reader["ItemCode"].ToString();
+                                item.ItemBrand = reader["Brand"].ToString();
+                                item.ItemDescription = reader["ReceiptDescription"].ToString();
+                                item.ItemLongDesc = reader["LongDescription"].ToString();
+                                item.ItemSize = reader["Size"].ToString();
+                                item.ItemCategory = reader["Category"].ToString();
+                                item.ItemReport = reader["Report"].ToString();
+                                item.ItemSubdepartment = reader["Subdepartment"].ToString();
+
+                                if (reader["Scalable"].ToString().Equals("0") || reader["Scalable"].ToString().Equals(""))
+                                {
+                                    item.ItemScaled = "No";
+                                }
+                                else if (reader["Scalable"].ToString().Equals("1"))
+                                {
+                                    item.ItemScaled = "Yes";
+                                }
+
+                                if (reader["Tax"].ToString().Equals("0") || reader["Tax"].ToString().Equals(""))
+                                {
+                                    item.ItemTaxed = "No";
+                                }
+                                else if (reader["Tax"].ToString().Equals("1"))
+                                {
+                                    item.ItemTaxed = "Yes";
+                                }
+
+                                if (reader["FoodStamp"].ToString().Equals("0") || reader["FoodStamp"].ToString().Equals(""))
+                                {
+                                    item.ItemFoodStamp = "No";
+                                }
+                                else if (reader["Tax"].ToString().Equals("1"))
+                                {
+                                    item.ItemFoodStamp = "Yes";
+                                }
+
+                                if (Decimal.TryParse(reader["RegularPrice"].ToString(), out tempDecimal))
+                                {
+                                    item.ItemRegPrice = tempDecimal;
+                                }
+                                else item.ItemRegPrice = 0;
+
+                                if (!reader["TPRPrice"].ToString().Equals(""))
+                                {
+                                    if (Decimal.TryParse(reader["TPRPrice"].ToString(), out tempDecimal))
+                                    {
+                                        item.ItemTPR = tempDecimal;
+                                    }
+                                    else item.ItemTPR = 0;
+
+                                    item.ItemStartTPR = reader["TPRStart"].ToString();
+                                    item.ItemEndTPR = reader["TPREnd"].ToString();
+                                    if (Decimal.TryParse(reader["PackageTPR"].ToString(), out tempDecimal))
+                                    {
+                                        item.ItemPkgTPR = tempDecimal;
+                                    }
+                                    else item.ItemPkgTPR = 0;
+                                }
+
+                                if (Decimal.TryParse(reader["PackagePrice"].ToString(), out tempDecimal))
+                                {
+                                    item.ItemPkgPrice = tempDecimal;
+                                }
+                                else item.ItemPkgPrice = 0;
+
+                                if (!reader["ActiveVendor"].ToString().Equals(""))
+                                {
+                                    item.ItemVendor = reader["ActiveVendor"].ToString();
+                                    if (!reader["VendorCode"].ToString().Equals(""))
+                                    {
+                                        item.ItemVendorCode = reader["VendorCode"].ToString();
+                                    }
+                                    if (!reader["Cost"].ToString().Equals(""))
+                                    {
+                                        if (Decimal.TryParse(reader["Cost"].ToString(), out tempDecimal))
+                                        {
+                                            item.ItemCost = tempDecimal;
+                                        }
+                                        else item.ItemCost = 0;
+                                        if (item.ItemCost != 0)
+                                        {
+                                            if (item.ItemRegPrice != 0)
+                                            {
+                                                item.ItemMargin = ((item.ItemRegPrice - item.ItemCost) / item.ItemRegPrice) * 100;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                // Add results to list
+                                report.Add(item);
+                            }
+                        }
+                        else item.ItemCode = "Item Not Found";
+
+                        // Close connection to SQL server
+                        con.Close();
+                    }
+                }
+
+                // Add totals to ViewBag to be used in report display
+                ViewBag.store = storeDescription;
+
+                return PartialView("_ItemLookupByBrand", report);
             }
             else
             {
