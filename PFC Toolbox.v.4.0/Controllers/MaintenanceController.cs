16c1ef5ad44749af58e68010ccce7e77c27dc8bf
Jeremy Anderson <52007940+SnackMeUp@users.noreply.github.com>
2020-03-03 17:01:13 -0600

    Update MaintenanceController.cs

--- PFC Toolbox.v.4.0/Controllers/MaintenanceController.cs
+++ PFC Toolbox.v.4.0/Controllers/MaintenanceController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PFC_Toolbox.v._4._0.Controllers
 {
     public class MaintenanceController : Controller
     {
         // GET: Maintenance
+        [Authorize] // Restricts the page to logged in users only
         public ActionResult Index()
         {
             return View();
@@ -22,12 +19,14 @@ namespace PFC_Toolbox.v._4._0.Controllers
         }
 
         // GET: /Maintenance/ProductUpdates
+        [Authorize] // Restricts the page to logged in users only
         public ActionResult ProductUpdates()
         {
             return View();
         }
 
         // GET: /Maintenance/NewProducts
+        [Authorize] // Restricts the page to logged in users only
         public ActionResult NewProducts()
         {
             return View();
