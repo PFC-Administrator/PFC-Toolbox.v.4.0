39839549567a7a2a1c406349b52925bab8ce0ef5
Jeremy Anderson <52007940+SnackMeUp@users.noreply.github.com>
2020-03-26 18:17:49 -0500

    Update PLUController.cs

--- PFC Toolbox.v.4.0/Controllers/Maintenance/PLUController.cs
+++ PFC Toolbox.v.4.0/Controllers/Maintenance/PLUController.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Net.Http.Formatting;
+﻿using System.Configuration;
 using System.Web;
 using System.Web.Http;
 using DataTables;
-using Newtonsoft.Json;
-using PFC_Toolbox.v._4._0.Extensions;
-using PFC_Toolbox.v._4._0.Models;
 
 namespace PFC_Toolbox.v._4._0.Controllers
 {
     public class PLUController : ApiController
     {
-        [Route("Maintenance/api/GetNewPLU")]
+        // GET: PLUs -- get all PLU/UPCs within specified range.
+        [Route("api/GetPLUUPCs")]
         [HttpGet]
         [HttpPost]
         public IHttpActionResult ProductUpdates(string lowerBound, string upperBound)
@@ -27,6 +22,44 @@ namespace PFC_Toolbox.v._4._0.Controllers
                     ).Where(q => q.Where("OBJ_TAB.F01", "(SELECT OBJ_TAB.F01 FROM OBJ_TAB WHERE OBJ_TAB.F01 BETWEEN '" + lowerBound + "' AND '" + upperBound + "')", "IN", false))
                      .Process(request)
                      .Data();
+
+                return Json(response);
+            }
+        }
+
+        // GET: PLUs -- add new reference UPC.
+        [Route("api/GetReferenceUPCs")]
+        [HttpGet]
+        public IHttpActionResult GetReferenceUPCs()
+        {
+            var request = HttpContext.Current.Request;
+
+            using (var db1 = new Database("sqlserver", ConfigurationManager.ConnectionStrings["ToolboxConnection"].ConnectionString))
+            {
+                var response = new Editor(db1, "ReferenceUPCs", "F01")
+                    .Field(new Field("ReferenceUPCs.F01")
+                    )
+                    .Process(request)
+                    .Data();
+
+                return Json(response);
+            }
+        }
+
+        // POST: PLUs -- add new reference UPC.
+        [Route("api/CreateReferenceUPC")]
+        [HttpPost]
+        public IHttpActionResult CreateReferenceUPC([FromBody] string F01)
+        {
+            var request = HttpContext.Current.Request;
+
+            using (var db1 = new Database("sqlserver", ConfigurationManager.ConnectionStrings["ToolboxConnection"].ConnectionString))
+            {
+                var response = new Editor(db1, "ReferenceUPCs", "F01")
+                    .Field(new Field("ReferenceUPCs.F01")
+                    )
+                    .Process(request)
+                    .Data();
 
                 return Json(response);
             }
