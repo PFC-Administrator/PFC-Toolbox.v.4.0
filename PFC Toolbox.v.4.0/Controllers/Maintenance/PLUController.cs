42c3d7433a448ff873a5c92ca7c9c4cab806ca64
Jeremy Anderson <52007940+SnackMeUp@users.noreply.github.com>
2020-03-28 16:48:36 -0500

    Finalized Get New PLU function

--- PFC Toolbox.v.4.0/Controllers/Maintenance/PLUController.cs
+++ PFC Toolbox.v.4.0/Controllers/Maintenance/PLUController.cs
@@ -11,7 +11,7 @@ namespace PFC_Toolbox.v._4._0.Controllers
         [Route("api/GetPLUUPCs")]
         [HttpGet]
         [HttpPost]
-        public IHttpActionResult ProductUpdates(string lowerBound, string upperBound)
+        public IHttpActionResult ProductUpdates(string lowerBound, string upperBound, string lowerBound2, string upperBound2)
         {
             var request = HttpContext.Current.Request;
 
@@ -19,7 +19,7 @@ namespace PFC_Toolbox.v._4._0.Controllers
             {
                 var response = new Editor(db1, "OBJ_TAB", "F01")
                     .Field(new Field("OBJ_TAB.F01")
-                    ).Where(q => q.Where("OBJ_TAB.F01", "(SELECT OBJ_TAB.F01 FROM OBJ_TAB WHERE OBJ_TAB.F01 BETWEEN '" + lowerBound + "' AND '" + upperBound + "')", "IN", false))
+                    ).Where(q => q.Where("OBJ_TAB.F01", "(SELECT OBJ_TAB.F01 FROM OBJ_TAB WHERE OBJ_TAB.F01 BETWEEN '" + lowerBound + "' AND '" + upperBound + "' OR OBJ_TAB.F01 BETWEEN '" + lowerBound2 + "' AND '" + upperBound2 + "')", "IN", false))
                      .Process(request)
                      .Data();
 
