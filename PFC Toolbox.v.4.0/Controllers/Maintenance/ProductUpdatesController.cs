44e20f7e2292e1df074434ec80fcbf57cacb9ebc
Jeremy Anderson <52007940+SnackMeUp@users.noreply.github.com>
2020-05-28 14:23:02 -0500

    Update ProductUpdatesController.cs

--- PFC Toolbox.v.4.0/Controllers/Maintenance/ProductUpdatesController.cs
+++ PFC Toolbox.v.4.0/Controllers/Maintenance/ProductUpdatesController.cs
@@ -8,8 +8,6 @@ namespace PFC_Toolbox.v._4._0.Controllers
 {
     public class ProductUpdatesController : ApiController
     {
-
-
         [Route("api/ProductMaintenance")]
         [HttpGet]
         [HttpPost]
